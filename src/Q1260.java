91373108a08732fba0db0c724703abf8c76ed0c8
debbs061 <gyr7220@naver.com>
2021-05-01 06:40:12 +0900

    BFS_DFS

---
+++ src/Q1260.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q1260 {
+
+    static ArrayList<Integer>[] a;
+    static boolean c[];
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        c[start] = true;
+        while (!q.isEmpty()) {
+            int x = q.remove();
+            System.out.print(x + " ");
+            for (int y : a[x]) {
+                if (c[y] == false) {
+                    c[y] = true;
+                    q.add(y);
+                }
+            }
+        }
+
+    }
+
+    public static void dfs(int x) {
+        if (c[x]) {
+            return;
+        }
+        c[x] = true;
+        System.out.print(x + " ");
+        for (int y : a[x]) {
+            if (c[y] == false) {
+                dfs(y);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 정점
+        int m = Integer.parseInt(st.nextToken()); // 간선
+        int start = Integer.parseInt(st.nextToken()); // 탐색 시작 번호
+        a = (ArrayList<Integer>[]) new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            a[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            a[u].add(v);
+            a[v].add(u);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(a[i]);
+        }
+
+        c = new boolean[n + 1];
+        dfs(start);
+        System.out.println();
+
+        c = new boolean[n + 1];
+        bfs(start);
+        System.out.println();
+
+    }
+}
