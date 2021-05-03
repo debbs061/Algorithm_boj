160c8dba0bcf1c7aaf55638f49c4bc97d2d5a84f
debbs061 <gyr7220@naver.com>
2021-05-04 02:44:02 +0900

    순열

---
+++ src/Q1010.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1010 {
+    static int[][] dp = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            // M개 중 N개를 뽑는 경우의 수
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            sb.append(combi(m, n)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    static int combi(int n, int r) {
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+
+        if (n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+
+        return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+    }
+}
