20fdea0698c7e7c4fff36158c35048792df7afe1
debbs061 <gyr7220@naver.com>
2021-09-24 07:43:34 +0900

    reverse print

---
+++ src/etc/Q4.java
@@ -0,0 +1,37 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Q4 {
+
+    public void reversePrint(String str) {
+        String reverseStr = "";
+        char[] t = str.toCharArray();
+        for (int i = 0; i < t.length / 2; i++) {
+            char tmp = t[i];
+            t[i] = t[t.length-1-i];
+            t[t.length-1-i] = tmp;
+        }
+        reverseStr = String.copyValueOf(t);
+
+        System.out.println(reverseStr);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Q4 q = new Q4();
+        String s[] = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            s[i] = br.readLine();
+        }
+        for (int i = 0; i < n; i++) {
+            q.reversePrint(s[i]);
+        }
+
+    }
+}
