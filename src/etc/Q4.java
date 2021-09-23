26cbf1fb5fe1dfd9489d05de608cc5ef1a32776e
debbs061 <gyr7220@naver.com>
2021-09-24 07:56:09 +0900

    reverse print

--- src/etc/Q4.java
+++ src/etc/Q4.java
@@ -7,17 +7,23 @@ import java.util.Scanner;
 
 public class Q4 {
 
+//    public void reversePrint(String str) {
+//        String reverseStr = "";
+//        char[] t = str.toCharArray();
+//        for (int i = 0; i < t.length / 2; i++) {
+//            char tmp = t[i];
+//            t[i] = t[t.length-1-i];
+//            t[t.length-1-i] = tmp;
+//        }
+//        reverseStr = String.copyValueOf(t);
+//
+//        System.out.println(reverseStr);
+//    }
+
     public void reversePrint(String str) {
         String reverseStr = "";
-        char[] t = str.toCharArray();
-        for (int i = 0; i < t.length / 2; i++) {
-            char tmp = t[i];
-            t[i] = t[t.length-1-i];
-            t[t.length-1-i] = tmp;
-        }
-        reverseStr = String.copyValueOf(t);
-
-        System.out.println(reverseStr);
+        StringBuilder s = new StringBuilder(str);
+        System.out.println(s.reverse());
     }
 
     public static void main(String[] args) throws IOException {
