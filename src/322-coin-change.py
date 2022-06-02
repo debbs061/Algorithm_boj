c3f9ee0ef626ebdf163515930d6299783f9bc3dd
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2022-06-02 22:38:56 +0900

    Add files via upload

---
+++ src/322-coin-change.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [1e9] * (amount + 1)  # the fewest number of coins
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        return dp[amount] if dp[amount] != 1e9 else -1
