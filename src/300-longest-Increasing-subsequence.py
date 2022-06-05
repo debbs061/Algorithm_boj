bd5f8c661d836d511a8949cad5709de2e468f149
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2022-06-05 10:55:37 +0900

    Add files via upload

---
+++ src/300-longest-Increasing-subsequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1] * (len(nums))
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
