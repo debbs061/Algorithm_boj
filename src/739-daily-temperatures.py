07f091d7fe86c094aea27a4fd1d8450abdf696aa
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2022-06-23 23:03:17 +0900

    Add files via upload

---
+++ src/739-daily-temperatures.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        res = [0] * len(temperatures)
+        stack = []  # pair : [temperature, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackIdx = stack.pop()
+                res[stackIdx] = i - stackIdx
+            stack.append([t, i])
+
+        return res
