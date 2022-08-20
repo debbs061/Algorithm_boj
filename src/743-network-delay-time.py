fdc4c9280a995f768dd7e31e63f523e9bdd3d18a
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2022-08-20 10:04:11 +0900

    Add files via upload

---
+++ src/743-network-delay-time.py
@@ -0,0 +1,32 @@
+import collections
+import heapq
+
+
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
+        """
+        :type times: List[List[int]]
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        edges = collections.defaultdict(list)
+        for u, v, w in times:
+            edges[u].append((v, w))
+
+        minHeap = [(0, k)]  # k node부터 시작
+        visit = set()
+        t = 0
+
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            if n1 in visit:
+                continue
+            visit.add(n1)
+            t = max(t, w1)
+
+            for n2, w2 in edges[n1]:
+                if n2 not in visit:
+                    heapq.heappush(minHeap, (w1 + w2, n2))
+
+        return t if len(visit) == n else -1
