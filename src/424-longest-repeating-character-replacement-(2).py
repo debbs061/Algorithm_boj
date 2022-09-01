a62c360bcf71e8162c8210955f848b9d0cbf01a3
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2022-09-01 21:05:43 +0900

    Add files via upload

---
+++ src/424-longest-repeating-character-replacement-(2).py
@@ -0,0 +1,20 @@
+import collections
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+        l = 0
+        maxf = 0
+        # return the length of the longest substring containing the same letter
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r], 0) + 1
+            maxf = max(maxf, count[s[r]])
+
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+
+        return res
