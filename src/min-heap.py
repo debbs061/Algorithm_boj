992825306bf3ec95e4dffc90a25918fa5930e006
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2022-12-05 22:52:22 +0900

    Add files via upload

---
+++ src/min-heap.py
@@ -0,0 +1,62 @@
+class Heap:
+    def __init__(self):
+        self.heap = [0]
+
+    def push(self, val):
+        self.heap.append(val)
+        i = len(self.heap) - 1
+
+        # Percolate up
+        while i > 1 and self.heap[i] < self.heap[i // 2]:
+            self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
+
+    def pop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
+
+        res = self.heap[1]
+        self.heap[1] = self.heap.pop()
+        i = 1
+        # Percolate down
+        while 2 * i < len(self.heap):
+            # Swap right child
+            if (2 * i + 1 < len(self.heap) and self.heap[2 * i + 1] < self.heap[2 * i] and self.heap[2 * i + 1] <
+                    self.heap[i]):
+                self.heap[i], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[i]
+                i = 2 * i + 1
+            # Swap left child
+            elif self.heap[i] > self.heap[2 * i]:
+                self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
+                i = 2 * i
+            else:
+                break
+        return res
+
+    def top(self):
+        if len(self.heap) > 1:
+            return self.heap[1]
+        return None
+
+    def heapify(self, arr):
+        # 0-th position is moved to the end
+        arr.append(arr[0])
+
+        self.heap = arr
+        cur = (len(self.heap) - 1) // 2
+        while cur > 0:
+            # Percolate down
+            i = cur
+            while 2 * i < len(self.heap):
+                # Swap right child
+                if (2 * i + 1 < len(self.heap) and self.heap[2 * i + 1] < self.heap[2 * i] and self.heap[2 * i + 1] <
+                        self.heap[i]):
+                    self.heap[i], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[i]
+                    i = 2 * i + 1
+                # Swap left child
+                elif self.heap[i] > self.heap[2 * i]:
+                    self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
+                else:
+                    break
+            cur -= 1
