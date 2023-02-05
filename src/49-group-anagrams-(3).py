386d5226eddf8e9224136280141fcde1e4dc1536
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2023-02-06 07:07:30 +0900

    Add files via upload

---
+++ src/49-group-anagrams-(3).py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import Counter, defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        res = defaultdict(list)
+
+        for str in strs:
+            count = [0] * 26
+            for ch in str:
+                count[ord(ch) - ord("a")] += 1
+            res[tuple(count)].append(str)
+
+        return res.values()
+
