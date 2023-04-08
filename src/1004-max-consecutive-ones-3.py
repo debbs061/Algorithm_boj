2a65fc60fc821173cad1136f20fb217ffde8b8cb
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2023-04-08 21:56:54 +0900

    Add files via upload

---
+++ src/1004-max-consecutive-ones-3.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def longestOnes(self, nums, k):
+        l = 0
+        for r in range(len(nums)):
+            k -= 1 - nums[r]
+            if k < 0:
+                k += 1 - nums[l]
+                l += 1
+        return r - l + 1
+
