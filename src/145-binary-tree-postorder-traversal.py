b2ea099c7357804490c774d731b6f25c94c51550
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2023-07-22 18:07:10 +0900

    Add files via upload

---
+++ src/145-binary-tree-postorder-traversal.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def recurse(node):
+            if not node:
+                return
+            recurse(node.left)
+            recurse(node.right)
+            ans.append(node.val)
+
+        recurse(root)
+        return ans
+
